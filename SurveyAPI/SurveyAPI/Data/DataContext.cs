cc67b7dd97bc90d8baa6ee508b2a76325142250c
DownyDowner <boris.vancauter@gmail.com>
2024-11-25 10:04:40 +0100

    Add: adding entities and PostgreSQL database configuration

---
+++ SurveyAPI/SurveyAPI/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyAPI.Entities;
+
+namespace SurveyAPI.Data {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options): base(options) {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<QuestionEntity>(builder => {
+                builder.HasKey(e => e.Id);
+                builder.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+
+            modelBuilder.Entity<ChoiceEntity>(builder => {
+                builder.HasKey(e => e.Id);
+                builder.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                builder.HasOne(e => e.Question)
+                    .WithMany(e => e.Choices)
+                    .HasForeignKey(e => e.IdQuestion);
+            });
+        }
+
+        public DbSet<QuestionEntity> Questions { get; set; }
+        public DbSet<ChoiceEntity> Choices { get; set; }
+    }
+}
---
+++ SurveyAPI/SurveyAPI/Entities/QuestionEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace SurveyAPI.Entities {
+    public class QuestionEntity {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime BeginDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public bool Multiple { get; set; } = false;
+
+        public ICollection<ChoiceEntity>? Choices { get; set; }
+    }
+}
