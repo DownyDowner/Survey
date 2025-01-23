cb43d97b4343187534a762469f50c68c59513329
DownyDowner <bobo.vancauter@live.fr>
2025-01-23 09:12:39 +0100

    Feat: display active question list when user is logged in

--- front-vue/src/views/questions/ActiveQuestions.vue
+++ front-vue/src/views/questions/ActiveQuestions.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>Active Questions</div>
+  <v-list>
+    <v-list-item
+      v-for="question in questionStore.activeQuestions"
+      :key="question.id"
+      :title="question.name"
+    />
+  </v-list>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useQuestionStore } from "../../stores/question";
+
+const questionStore = useQuestionStore();
+
+onMounted(async () => {
+  await loadActiveQuestions();
+});
+
+async function loadActiveQuestions() {
+  await questionStore.getAllActiveQuestions();
+}
+</script>
