d7ed189696d14b5f6f6d111c3f49245e615b9b23
DownyDowner <bobo.vancauter@live.fr>
2025-02-05 13:36:51 +0100

    Add(ActiveQuestions): Added date range display and an append button for each item

--- front-vue/src/views/questions/ActiveQuestions.vue
+++ front-vue/src/views/questions/ActiveQuestions.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-list>
-    <v-list-item
-      v-for="question in questionStore.activeQuestions"
-      :key="question.id"
-      :title="question.name"
-    />
-  </v-list>
+  <v-container>
+    <v-list>
+      <v-list-item
+        v-for="question in questionStore.activeQuestions"
+        :key="question.id"
+        :title="question.name"
+        :subtitle="formatDateRange(question.beginDate, question.endDate)"
+      >
+        <template v-slot:append>
+          <v-btn color="primary" icon="mdi-chevron-right" variant="text" />
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -20,5 +27,12 @@ onMounted(async () => {
 
 async function loadActiveQuestions() {
   await questionStore.getAllActiveQuestions();
+}
+
+function formatDateRange(beginDate: string, endDate: string): string {
+  const begin = new Date(beginDate).toLocaleDateString();
+  const end = new Date(endDate).toLocaleDateString();
+
+  return `From ${begin} to ${end}`;
 }
 </script>
