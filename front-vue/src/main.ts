d8ed2ccbc13cdcd3dfbc931ac91c56b43b56757b
DownyDowner <bobo.vancauter@live.fr>
2025-02-28 09:42:38 +0100

    chore: add vue-toastification dependency

--- front-vue/src/main.ts
+++ front-vue/src/main.ts
@@ -8,11 +8,20 @@ import router from "./router";
 
 import "@mdi/font/css/materialdesignicons.css";
 import vuetify from "../plugins/vuetify";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(vuetify);
+app.use(Toast, {
+  position: POSITION.BOTTOM_RIGHT,
+  timeout: 3000,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.5,
+});
 
 app.mount("#app");
