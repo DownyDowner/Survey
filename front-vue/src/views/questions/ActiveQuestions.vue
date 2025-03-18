11a99f61b591595fb5acdc4dcf195be6cdcabc48
DownyDowner <bobo.vancauter@live.fr>
2025-03-18 09:27:25 +0100

    Add: autocomplete to the active questions list

--- front-vue/src/views/questions/ActiveQuestions.vue
+++ front-vue/src/views/questions/ActiveQuestions.vue
@@ -1,15 +1,31 @@
 <template>
   <v-container>
-    <QuestionsList
-      ref="questionList"
-      :questions="questionStore.activeQuestions"
-      :is-closed="false"
-    />
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-autocomplete
+          clearable
+          label="Search"
+          :items="questionStore.activeQuestions"
+          hide-details
+          item-title="name"
+          rounded
+          prepend-inner-icon="mdi-magnify"
+          v-model:search="searchQuery"
+        />
+      </v-col>
+      <v-col cols="12">
+        <QuestionsList
+          ref="questionList"
+          :questions="filteredQuestions"
+          :is-closed="false"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useQuestionStore } from "../../stores/question";
 import QuestionsList from "./components/QuestionsList.vue";
 import { useNotificationStore } from "../../stores/notification";
@@ -18,6 +34,14 @@ const questionStore = useQuestionStore();
 const notificationStore = useNotificationStore();
 
 const questionList = ref<InstanceType<typeof QuestionsList> | null>(null);
+const searchQuery = ref("");
+
+const filteredQuestions = computed(() => {
+  if (!searchQuery.value) return questionStore.activeQuestions;
+  return questionStore.activeQuestions.filter((q) =>
+    q.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+});
 
 onMounted(async () => {
   await loadActiveQuestions();
